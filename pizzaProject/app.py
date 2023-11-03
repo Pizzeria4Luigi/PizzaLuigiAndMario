a6a86a3323ed62aec98663b36c799b9b1151c967
Adrians Daleckis <adriansdaleckis@gmail.com>
2023-11-03 13:40:57 +0100

    Now to register user needs to confirm his email

--- pizzaProject/app.py
+++ pizzaProject/app.py
@@ -1,9 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for, json, session, flash
 import os
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
 file_path = os.path.join(os.path.dirname(__file__), "currentOrder.txt")
 app.secret_key = "super secret key"
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465  # or the appropriate port
+app.config['MAIL_USERNAME'] = 'adriansdaleckis@gmail.com'
+app.config['MAIL_PASSWORD'] = 'dqdz iiml mnvo nrdu'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+# Function to generate the confirmation token
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(app.secret_key)
+    return serializer.dumps(email, salt='email-confirm')
+
+# Function to confirm the token
+def confirm_token(token, expiration=3600):  # Token expiration time set to 1 hour (3600 seconds) by default
+    serializer = URLSafeTimedSerializer(app.secret_key)
+    try:
+        email = serializer.loads(
+            token,
+            salt='email-confirm',
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
 
 @app.route('/',methods=['GET', 'POST'])
 def Main():
@@ -11,6 +40,16 @@ def Main():
         return render_template('PizzaMenu.html', username=session['username'])
     else:
         return render_template('PizzaMenu.html')
+    
+@app.route('/send-conf-mail', methods=['GET', 'POST'])
+def send_conf_mail():
+    if request.method == 'POST':
+        email = request.form['email']
+        message = Message('Email Confirmation', sender='your_email@example.com', recipients=[email])
+        message.body = 'This is a confirmation email to confirm the existence of the email.'
+        mail.send(message)
+        return 'Email sent successfully!'
+    return render_template('email_form.html')  # Create a template named 'email_form.html'
 
 @app.route('/order', methods=['POST'])
 def order_pizza():
@@ -77,6 +116,14 @@ def Register_page1():
             d['email']=attempted_email
             data.append(d)
 
+            token = generate_confirmation_token(attempted_email)  # You will need to implement this function
+            confirm_url = f'http://127.0.0.1:5000/confirm_email/{token}'  # Replace with your website URL
+            html = render_template('confirmation_email.html', confirm_url=confirm_url)
+            message = Message('Account Confirmation', sender='adriansdaleckis@gmail.com', recipients = ['adriansdaleckis@gmail.com'])
+            message.html = html
+            mail.send(message)
+
+            session['email_confitmed'] = False
 
             if request.form['Username'] == '' or request.form['Password'] == '' or request.form['Email'] == '':
                return 'Invalid Credentials. Please try again.' 
@@ -86,6 +133,17 @@ def Register_page1():
                 return render_template('LoginPage.html')
         else:
             return render_template('RegisterPage.html')
+
+@app.route('/confirm_email/<token>', methods=['GET'])
+def confirm_email(token):
+    email = confirm_token(token)  # Use the confirm_token function you previously defined
+    if email:
+        # Here you can add code to update the user's account as confirmed or perform any other necessary actions.
+        session['email_confitmed'] = True
+        return f'Thank you for confirming your email, {email}!'
+    else:
+        return 'The confirmation link is invalid or has expired.'
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
@@ -99,20 +157,30 @@ def login_page():
         
         with open('data.json', 'r') as outfile: 
             data = json.load(outfile)
-       
-        print(data)
         
-        for user in data:
-            if request.form['Username'] == user['user'] and request.form['Password'] == user['pass'] and request.form['Email'] == user['email']:
-                print(request.form['Username'])
-                session['username'] = request.form['Username']
-                return redirect(url_for('Main'))
-                #return 'SUCCESSFULLY LOGGEDIN'
-                #error = 'Invalid Credentials. Please try again.'
-            # return render_template('bcd.html')
-        #return render_template('login.html')
+        print(session['email_confitmed'])
+       
+        if session['email_confitmed'] == True:
+            for user in data:
+                if request.form['Username'] == user['user'] and request.form['Password'] == user['pass'] and request.form['Email'] == user['email']:
+                    print(request.form['Username'])
+                    session['username'] = request.form['Username']
+                    return redirect(url_for('Main'))
+                    #return 'SUCCESSFULLY LOGGEDIN'
+                    #error = 'Invalid Credentials. Please try again.'
+                # return render_template('bcd.html')
+            #return render_template('login.html')
+        else:
+            flash(f'You have not confirmed your email.')
+            return render_template('LoginPage.html')
 
     return render_template('LoginPage.html')
+
+@app.route('/logout')
+def logout():
+    # Clear the email stored in the session object
+    session.pop('username', default=None)
+    return redirect(url_for('Main'))
 
 @app.route('/orders', methods=['GET', 'POST'])
 def orders():
